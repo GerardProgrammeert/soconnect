e98dcaf4430dacdd576d22174840564e310f032e
Gerard van Hattem <gwjvanhattem@gmail.com>
2022-04-21 14:17:46 +0200

    Generate env and set env variables via docker composer

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,18 @@
-#!/bin/sh
-sleep 60
+#!/bin/bash
+sleep 30
 
+cp .env.example .env
+DB_DATABASE="sc"
+DB_USERNAME="root"
+DB_PASSWORD="example"
+echo "$MYSQL_ROOT_PASSWORD"
+
+sed -i "s/DB_DATABASE=.*/DB_DATABASE=$MYSQL_DATABASE/" .env
+sed -i "s/DB_USERNAME=.*/DB_USERNAME=$MYSQL_USER/" .env
+sed -i "s/DB_PASSWORD=.*/DB_PASSWORD=$MYSQL_ROOT_PASSWORD/" .env
+
+php artisan key:generate
 php artisan migrate
+
 
 while true; do sleep 1; done
