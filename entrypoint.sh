5a73aa23e1804474eec88dfa8c6beee07791b74f
Gerard van Hattem <gwjvanhattem@gmail.com>
2022-04-21 21:09:02 +0200

    Added dockerfile/dockercompose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-echo "asdsdsds"
+echo "Entrypoint"
+echo "Run Composer install"
 composer install
 composer dump-autoload
+echo "Wait for a while"
 sleep 30
+echo "Generate env file"
 cp .env.example .env
-DB_DATABASE="sc"
-DB_USERNAME="root"
-DB_PASSWORD="example"
-echo "$MYSQL_ROOT_PASSWORD"
 
+sed -i "s/DB_HOST=.*/DB_HOST=$MYSQL_HOST/" .env
 sed -i "s/DB_DATABASE=.*/DB_DATABASE=$MYSQL_DATABASE/" .env
 sed -i "s/DB_USERNAME=.*/DB_USERNAME=$MYSQL_USER/" .env
-sed -i "s/DB_PASSWORD=.*/DB_PASSWORD=$MYSQL_ROOT_PASSWORD/" .env
+sed -i "s/DB_PASSWORD=.*/DB_PASSWORD=$MYSQL_PASSWORD/" .env
 
 php artisan key:generate
 php artisan migrate
